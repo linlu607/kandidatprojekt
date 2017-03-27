47af3ebb48b95a7f6f442d264c33168505fba62a
olani913 <olavnilsson@gmail.com>
2017-03-27 19:18:06 +0200

    Update statsclass with timestamps

--- UpdateClicks.py
+++ UpdateClicks.py
@@ -1,27 +1,30 @@
 import xlwt
 import ast
-import re
+import datetime
+import time
+colTime=1
+while True:
+    filename = "./data/newsJson.txt"
 
-filename = "./data/newsJson.txt"
-
-with open(filename, 'r') as file:
-    data=file.read().split('\n')#.replace('\n', '')
-
-
-book = xlwt.Workbook(encoding="utf-8")
-sheet1 = book.add_sheet("Sheet 1")
-sheet1.write(0, 0, "url")
-sheet1.write(0, 1, "Antal click")
-tmp=1
-for line in data:
-    if line!='':
-        dataDict = ast.literal_eval(line)
-        print(dataDict["long_url"])
-        url=dataDict["long_url"]
-        clicks=dataDict["global_clicks"]
-        sheet1.write(tmp, 0, url)
-        sheet1.write(tmp, 1, clicks)
-        tmp=tmp+1
+    with open(filename, 'r') as file:
+        data=file.read().split('\n')#.replace('\n', '')
 
 
-book.save("trial.xls")
+    book = xlwt.Workbook(encoding="utf-8")
+    sheet1 = book.add_sheet("Sheet 1")
+    sheet1.write(0, 0, "url")
+    sheet1.write(0, 1, "Antal click vid tiden " + datetime.datetime.now)
+    tmp=1
+    for line in data:
+        if line!='':
+            dataDict = ast.literal_eval(line)
+            print(dataDict["long_url"])
+            url=dataDict["long_url"]
+            clicks=dataDict["global_clicks"]
+            sheet1.write(tmp, 0, url)
+            sheet1.write(tmp, colTime, clicks)
+            tmp=tmp+1
+
+    book.save("ResultsBitly.xls")
+    time.sleep( 3600 )
+    colTime=colTime+1
