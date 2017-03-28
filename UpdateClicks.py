66eb27ee79b5095a37ceefcf98a22e1ec647e40a
olani913 <olavnilsson@gmail.com>
2017-03-28 10:09:02 +0200

    Update statsclass with timestamps

--- UpdateClicks.py
+++ UpdateClicks.py
@@ -1,26 +1,30 @@
-# -*- coding: cp1252 -*-
 import xlwt
 import ast
 import datetime
 import time
 colTime=1
 while True:
-    filename = "./data/newsJson.txt"
 
+    filename = "./data/newsJson.txt"
     with open(filename, 'r') as file:
-        data=file.read().split('\n')
+        data=file.read().split('\n')#.replace('\n', '')
 
 
     book = xlwt.Workbook(encoding="utf-8")
     sheet1 = book.add_sheet("Sheet 1")
-    sheet1.write(0, 0, "URL")
-    sheet1.write(0, 1, "Data inläst till excelarket: " + datetime.datetime.now)
+    sheet1.write(0, 0, "url")
+    localtime = time.asctime( time.localtime(time.time()) )
+    print localtime
+
+    sheet1.write(0, 1, "Antal click vid tiden " + localtime)
     tmp=1
     for line in data:
         if line!='':
             dataDict = ast.literal_eval(line)
-            print(dataDict["long_url"])
+
             url=dataDict["long_url"]
+            url=str(url).replace("http://","")
+            print(dataDict["long_url"])
             clicks=dataDict["global_clicks"]
             sheet1.write(tmp, 0, url)
             sheet1.write(tmp, colTime, clicks)
